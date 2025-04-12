f6302b440c46006b46292b898a0ff72a8fed20c4
windowsworldcartoon <165859366+windowsworldcartoon@users.noreply.github.com>
2025-04-12 21:25:18 +0000

    add all

---
+++ app/callback/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function  callback() {
+  
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+           <h1>windowsworld callbacks</h1>
+           <p>you can close this window</p>
+        </div>
+      );
+}
